6077c84c4edb68762b1a82045bfee0cf470a3e6b
dkuntz <david.kuntz22@gmail.com>
2016-11-09 19:32:51 +0100

    first commit

---
+++ app/src/main/java/com/example/user/applievent/WelcomePage.java
@@ -0,0 +1,14 @@
+package com.example.user.applievent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //test commit
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_page);
+    }
+}
